be366fa69315f8111b6c6e425e3869a58aa45339
Sefik17 <78441706+Sefik17@users.noreply.github.com>
2021-11-12 19:28:19 +0100

    Update client.lua

--- servermenu/client.lua
+++ servermenu/client.lua
@@ -1,11 +1,4 @@
-ESX = nil
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-end)
 
 RegisterCommand(Config.Command, function()
     SendNUIMessage({type = 'otvorimenu'})
